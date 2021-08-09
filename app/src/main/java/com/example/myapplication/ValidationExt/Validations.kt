3d2ed218282dc6596377292c5aeacf80f0730625
cpp-srashti <cpp-shrasti@mobiloitte.com>
2021-08-09 16:14:43 +0530

    1. done proper validations on SignUpActivity.kt,save profile 2. set topbar acc to given sizes like profile, filter 3.set icons of bottom bar  and made color change on click. 4. set logout button made responsive

--- app/src/main/java/com/example/myapplication/ValidationExt/Validations.kt
+++ app/src/main/java/com/example/myapplication/ValidationExt/Validations.kt
@@ -9,119 +9,126 @@ import com.example.myapplication.R
 import java.util.regex.Pattern
 
 object Validations : Activity() {
-     private val PASSWORD_PATTERN =
-         Pattern.compile(
-             "^" +
-                     "(?=.*[@#$%^&+=])" +  // at least 1 special character
-                     "(?=\\S+$)" +  // no white spaces
-                     ".{4,}" +  // at least 4 characters
-                     "$")
- var check:Boolean=true
-    fun required(value: String, msg:TextView){
-        if(value.length ==0){
+    private val PASSWORD_PATTERN =
+        Pattern.compile(
+            "^" +
+                    "(?=.*[@#$%^&+=])" +  // at least 1 special character
+                    "(?=\\S+$)" +  // no white spaces
+                    ".{4,}" +  // at least 4 characters
+                    "$"
+        )
+    var check: Boolean = true
+    fun required(value: String, msg: TextView): Boolean {
+        if (value.length == 0) {
             msg.visibility = View.VISIBLE
             msg.setText("Full Name input field is null")
-        }
-
-        else{
+            return false
+        } else {
             msg.setText("")
             msg.visibility = View.GONE
-
+            return true
         }
-
+        return true
     }
-     fun Email(value: String,msg:TextView ){
-         if (value.length==0){
-             msg.setText("Email Address input field is empty")
-             msg.visibility = View.VISIBLE
 
-         }
-         else if(!Patterns.EMAIL_ADDRESS.matcher(value).matches()){
-             msg.setText("Email Address input field is not valid")
-             msg.visibility = View.VISIBLE
+    fun Email(value: String, msg: TextView): Boolean {
+        if (value.length == 0) {
+            msg.setText("Email Address input field is empty")
+            msg.visibility = View.VISIBLE
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+            msg.setText("Email Address input field is not valid")
+            msg.visibility = View.VISIBLE
+            return false
+        } else {
+            msg.setText("")
+            msg.visibility = View.GONE
+            return true
+        }
+        return true
+    }
 
-         }
-         else{
-             msg.setText("")
-             msg.visibility = View.GONE
+    fun CheckPhoneNumber(value: String, msg: TextView): Boolean {
+        if (value.length == 0) {
+            msg.setText("phone number input field is empty")
+            msg.visibility = View.VISIBLE
+            return false
+        } else if (value.length >= 12) {
+            msg.setText("phone number length must not greater than 12")
+            msg.visibility = View.VISIBLE
+            return false
+        } else {
+            msg.setText("")
+            msg.visibility = View.GONE
+            return true
+        }
+        return true
+    }
 
-         }
-     }
+    fun Password(value: String, msg: TextView): Boolean {
+        if (value.length == 0) {
+            msg.setText("password input is empty")
+            msg.visibility = View.VISIBLE
+            return false
 
-     fun CheckPhoneNumber(value: String,msg: TextView){
-         if (value.length==0){
-             msg.setText("phone number input field is empty")
-             msg.visibility = View.VISIBLE
+        } else if (!PASSWORD_PATTERN.matcher(value).matches()) {
 
-         }
-         else if (value.length>=12){
-             msg.setText("phone number length must not greater than 12")
-             msg.visibility = View.VISIBLE
-
-         }
-         else{
-             msg.setText("")
-             msg.visibility = View.GONE
-
-         }
-
-     }
-
-     fun Password(value: String, msg: TextView){
-         if(value.length==0){
-             msg.setText("password input is empty")
-             msg.visibility = View.VISIBLE
-
-
-         }
-         else if(!PASSWORD_PATTERN.matcher(value).matches()){
-
-             msg.setText(" Password input field is not valid ")
-             msg.visibility = View.VISIBLE
-
-         }
-         else if (value.length>=8){
-             msg.setText("password length must not greater than 8")
-             msg.visibility = View.VISIBLE
-
-         }
-         else{
-             msg.setText("")
-             msg.visibility = View.GONE
-
-         }
-     }
+            msg.setText(" Password input field is not valid ")
+            msg.visibility = View.VISIBLE
+            return false
+        } else if (value.length >= 8) {
+            msg.setText("password length must not greater than 8")
+            msg.visibility = View.VISIBLE
+            return false
+        } else {
+            msg.setText("")
+            msg.visibility = View.GONE
+            return true
+        }
+        return true
+    }
 
 
-    fun EmailLogin(value: String,msg:TextView, mErrorMessage:TextView,mBackground:RelativeLayout):Boolean{
-        if (value.length==0){
+
+
+    fun EmailLogin(
+        value: String,
+        msg: TextView,
+        mErrorMessage: TextView,
+        mBackground: RelativeLayout
+    ): Boolean {
+        if (value.length == 0) {
             msg.setText("Email  Address input field is empty")
             msg.visibility = View.VISIBLE
             mBackground.visibility = View.VISIBLE
             mErrorMessage.setText("Email and Password is not valid")
             mBackground.setBackgroundResource(R.drawable.background_error)
-          return false
-        }
-        else if(!Patterns.EMAIL_ADDRESS.matcher(value).matches()){
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
             msg.setText("Email Address input field is not valid")
             msg.visibility = View.VISIBLE
             mBackground.visibility = View.VISIBLE
             mErrorMessage.setText("Email and Password is not valid")
             mBackground.setBackgroundResource(R.drawable.background_error)
-      return false
-        }
-        else{
+            return false
+        } else {
             msg.setText("")
             msg.visibility = View.GONE
             mBackground.visibility = View.VISIBLE
             mErrorMessage.setText("")
             mBackground.setBackgroundResource(R.drawable.drawable_back)
-       return true
+            return true
         }
-        return  true
+        return true
     }
-    fun PasswordLogin(value: String,msg:TextView, mErrorMessage:TextView,mBackground:RelativeLayout):Boolean{
-        if (value.length==0){
+
+    fun PasswordLogin(
+        value: String,
+        msg: TextView,
+        mErrorMessage: TextView,
+        mBackground: RelativeLayout
+    ): Boolean {
+        if (value.length == 0) {
             msg.setText("Password input field is empty")
             msg.visibility = View.VISIBLE
             mBackground.visibility = View.VISIBLE
@@ -137,23 +144,23 @@ object Validations : Activity() {
 //            mErrorMessage.setText("UserName and Password is not valid")
 //            mBackground.setBackgroundResource(R.drawable.background_error)
 //        }
-        else if (value.length<=6){
+        else if (value.length <= 6) {
             msg.setText("password length must not less than 6")
             msg.visibility = View.VISIBLE
             mBackground.visibility = View.VISIBLE
             mErrorMessage.setText("UserName and Password is not valid")
             mBackground.setBackgroundResource(R.drawable.background_error)
-          return false
-        }
-        else{
+            return false
+        } else {
             msg.setText("")
             msg.visibility = View.GONE
             mBackground.visibility = View.VISIBLE
             mErrorMessage.setText("")
             mBackground.setBackgroundResource(R.drawable.drawable_back)
-return true        }
-    return  true
+            return true
+        }
+        return true
     }
 
 
- }
+}
