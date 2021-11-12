07c85043b443bce7b8947d6c1f298d4fa1c7fc84
an-ajay <an-ajay@mobiloitte.com>
2021-11-12 18:21:03 +0530

    Implementation of notification design bug fix on screen:- user profile,edit profile, other profile

--- app/src/main/java/com/example/myapplication/Adaptor/MessageAdaptor.kt
+++ app/src/main/java/com/example/myapplication/Adaptor/MessageAdaptor.kt
@@ -12,25 +12,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.entity.Response.Messages
 
-class MessageAdaptor(var listdat: ArrayList<Messages>, var USERID: String) : RecyclerView.Adapter<MessageAdaptor.MyViewHolder>() {
-    inner class MyViewHolder(view: View):RecyclerView.ViewHolder(view){
-        var sendertext=view.findViewById<TextView>(R.id.sendertext)
-        var recivertext=view.findViewById<TextView>(R.id.recivertext)
-        var sender=view.findViewById<LinearLayout>(R.id.sender)
-        var reciver=view.findViewById<RelativeLayout>(R.id.reciver)
-
+class MessageAdaptor(var listdat: ArrayList<Messages>, var USERID: String) :
+    RecyclerView.Adapter<MessageAdaptor.MyViewHolder>() {
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var sendertext = view.findViewById<TextView>(R.id.sendertext)
+        var recivertext = view.findViewById<TextView>(R.id.recivertext)
+        var sender = view.findViewById<LinearLayout>(R.id.sender)
+        var reciver = view.findViewById<RelativeLayout>(R.id.reciver)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageAdaptor.MyViewHolder {
-        var v= LayoutInflater.from(parent.context).
-                inflate(R.layout.layout_recycler_chat,null)
+        var v = LayoutInflater.from(parent.context).inflate(R.layout.layout_recycler_chat, null)
         return MyViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: MessageAdaptor.MyViewHolder, position: Int)
-    {if(listdat.get(position).receiverId.equals(USERID))
-    {holder.sender.visibility=View.VISIBLE
-        holder.reciver.visibility=View.GONE
-        holder.sendertext.setText(listdat.get(position).message)
+    override fun onBindViewHolder(holder: MessageAdaptor.MyViewHolder, position: Int) {
+        if (listdat.get(position).receiverId.equals(USERID)) {
+            holder.sender.visibility = View.VISIBLE
+            holder.reciver.visibility = View.GONE
+            holder.sendertext.setText(listdat.get(position).message)
 //        val params = LinearLayout.LayoutParams(
 //            LinearLayout.LayoutParams.MATCH_PARENT,
 //            LinearLayout.LayoutParams.WRAP_CONTENT
@@ -39,17 +39,15 @@ class MessageAdaptor(var listdat: ArrayList<Messages>, var USERID: String) : Rec
 //            gravity = Gravity.RIGHT
 //        }
 //        holder.sender.setLayoutParams(params);
-    }
-        else
-    {
-        holder.sender.visibility=View.GONE
-        holder.reciver.visibility=View.VISIBLE
-        holder.recivertext.setText(listdat.get(position).message)
-    }
+        } else {
+            holder.sender.visibility = View.GONE
+            holder.reciver.visibility = View.VISIBLE
+            holder.recivertext.setText(listdat.get(position).message)
+        }
 
     }
 
     override fun getItemCount(): Int {
-    return listdat.size
+        return listdat.size
     }
 }
