d41cfdafddce9f3b4dc8843a97d696be643b6556
an-karan <an-karan@mobiloitte.com>
2021-11-19 12:53:12 +0530

    some changes in date format for comment time show

--- app/src/main/java/com/example/myapplication/util/DateFormat.kt
+++ app/src/main/java/com/example/myapplication/util/DateFormat.kt
@@ -11,7 +11,7 @@ class DateFormat {
         @SuppressLint("SimpleDateFormat")
         fun covertTimeOtherFormat(dataDate: String?): String? {
             var convTime = ""
-            val suffix = "ago."
+            val suffix = "ago"
             try {
                 val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
                 sdf.timeZone = TimeZone.getTimeZone("UTC")
@@ -19,8 +19,15 @@ class DateFormat {
                 //String time = getUtcTime(dataDate.substring(0,19));
                 //  String[] timeie=dataDate.split("\\.");
                 val date = sdf.parse(dataDate)
+                val calendar: Calendar = GregorianCalendar(date.year, date.month, date.day, date.hours, date.minutes, date.seconds)
                 val nowTime = Date()
                 val dateDiff = nowTime.time - date.time
+                val year = calendar[Calendar.YEAR]
+                val month = calendar[Calendar.MONTH]
+                val dayOfMonth = calendar[Calendar.DAY_OF_MONTH]
+                val dayOfWeek = calendar[Calendar.DAY_OF_WEEK]
+                val weekOfYear = calendar[Calendar.WEEK_OF_YEAR]
+                val weekOfMonth = calendar[Calendar.WEEK_OF_MONTH]
                 val second = TimeUnit.MILLISECONDS.toSeconds(dateDiff)
                 val minute = TimeUnit.MILLISECONDS.toMinutes(dateDiff)
                 val hour = TimeUnit.MILLISECONDS.toHours(dateDiff)
@@ -33,7 +40,9 @@ class DateFormat {
                 } else if (hour >= 24) {
                     val newSdf = SimpleDateFormat("yyyy-mm-dd HH:mm:ss")
                     newSdf.timeZone = TimeZone.getTimeZone("UTC")
-                    convTime = newSdf.format(date)
+//                    convTime = newSdf.format(date)
+                    convTime = "$dayOfMonth days $suffix"
+
                 }
             } catch (e: ParseException) {
                 e.printStackTrace()
